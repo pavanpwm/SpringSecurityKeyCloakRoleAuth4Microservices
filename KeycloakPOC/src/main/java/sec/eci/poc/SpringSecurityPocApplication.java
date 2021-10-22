a3020767a68c40761d5cd5f235c613bf15f217b3
Pavan BTD <pavanpwm@gmail.com>
2021-10-22 09:40:12 +0530

    Add files via upload

---
+++ KeycloakPOC/src/main/java/sec/eci/poc/SpringSecurityPocApplication.java
@@ -0,0 +1,13 @@
+package sec.eci.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityPocApplication.class, args);
+	}
+
+}
